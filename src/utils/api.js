915b3d196dda6dff2f3c5adf2d44e0b0378f91fe
Akash Mondal <111482722+akash52004@users.noreply.github.com>
2025-06-20 21:02:31 +0530

    api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = https://task-manager-1-92pe.onrender.com
 
 axios.defaults.baseURL = API_URL;
 
