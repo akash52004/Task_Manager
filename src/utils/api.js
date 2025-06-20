abe42f36af48af9993946e5b5fe29f22cfb49e20
Akash Mondal <111482722+akash52004@users.noreply.github.com>
2025-06-20 21:22:38 +0530

    api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
-axios.defaults.baseURL =https://task-manager-1-92pe.onrender.com;
+axios.defaults.baseURL = API_URL;
 
 export const taskAPI = {
   getTasks: () => axios.get('/api/tasks'),
